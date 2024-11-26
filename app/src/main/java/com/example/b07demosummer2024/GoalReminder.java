fdc412773f23c857cdda1d7edc5ab0d6fedfe9cc
Janif Kader <janifk4@gmail.com>
2024-11-26 01:14:49 -0500

    feat: added Eco Goals functionality (notifications, progress bar, reccomendations)

---
+++ app/src/main/java/com/example/b07demosummer2024/GoalReminder.java
@@ -0,0 +1,45 @@
+package com.example.b07demosummer2024.DailyActivity;
+
+import android.annotation.SuppressLint;
+import android.app.PendingIntent;
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+
+import androidx.core.app.ActivityCompat;
+import androidx.core.app.NotificationCompat;
+import androidx.core.app.NotificationManagerCompat;
+
+import com.example.b07demosummer2024.R;
+
+public class GoalReminder extends BroadcastReceiver {
+    @SuppressLint({"NotificationPermission", "MissingPermission"})
+    @Override
+    public void onReceive(Context context, Intent intent){
+        Intent i = new Intent(context, GoalPicker.class);
+        intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+        PendingIntent pendingIntent = PendingIntent.getActivity(context, 0, i, PendingIntent.FLAG_IMMUTABLE);
+
+        NotificationCompat.Builder builder = new NotificationCompat.Builder(context, "notify")
+                .setSmallIcon(R.drawable.ic_launcher)
+                .setContentTitle("Planetze")
+                .setContentText("Don't forget to complete and log your goals today!")
+                .setPriority(NotificationCompat.PRIORITY_DEFAULT)
+                .setContentIntent(pendingIntent)
+                .setAutoCancel(true);
+
+        NotificationManagerCompat notificationManager = NotificationManagerCompat.from(context);
+        if (ActivityCompat.checkSelfPermission(context, android.Manifest.permission.POST_NOTIFICATIONS) != PackageManager.PERMISSION_GRANTED) {
+            // TODO: Consider calling
+            //    ActivityCompat#requestPermissions
+            // here to request the missing permissions, and then overriding
+            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+            //                                          int[] grantResults)
+            // to handle the case where the user grants the permission. See the documentation
+            // for ActivityCompat#requestPermissions for more details.
+            return;
+        }
+        notificationManager.notify(200, builder.build());
+    }
+}
