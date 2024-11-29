0443a79915b020052a52f7e5cade5be761ed98cc
KKAPOOR30 <kshitij_kapoor@hotmail.com>
2024-11-28 20:24:42 -0500

    Filters added and work
    Filters have been added and scrolling works, But filtering and Search do not work together yet.

--- app/src/main/java/com/example/b07demosummer2024/HabitSearchActivity.java
+++ app/src/main/java/com/example/b07demosummer2024/HabitSearchActivity.java
@@ -5,6 +5,9 @@ import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.CheckBox;
 import android.widget.SearchView;
 import android.widget.Toast;
 
@@ -22,11 +25,32 @@ public class HabitSearchActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_habit_search);
 
+        CheckBox transport = findViewById(R.id.transportFilter);
+        CheckBox food = findViewById(R.id.foodFilter);
+        CheckBox electricity = findViewById(R.id.electricityFilter);
+        CheckBox shopping = findViewById(R.id.shoppingFilter);
+        CheckBox small = findViewById(R.id.smallFilter);
+        CheckBox medium = findViewById(R.id.mediumFilter);
+        CheckBox large = findViewById(R.id.largeFilter);
+        Button filter = findViewById(R.id.filterButton);
+
         recyclerView = findViewById(R.id.habitrecyclerview);
         SearchView searchview = findViewById(R.id.habitsearchView);
         searchview.clearFocus();
 
         habits = new ArrayList<>();
+
+        filter.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (transport.isChecked() || food.isChecked() || electricity.isChecked() || shopping.isChecked() || small.isChecked() || medium.isChecked() || large.isChecked()) {
+                    filterList(transport, food, electricity, shopping, small, medium, large);
+                } else {
+                    habitadaptor.setFilteredList(habits);
+                    recyclerView.setAdapter(habitadaptor);
+                }
+            }
+        });
 
         searchview.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
             @Override
@@ -46,12 +70,50 @@ public class HabitSearchActivity extends AppCompatActivity {
         habits.add(new Habit("3","Transportation","small","Did you run carpool to work?","Carpooling with colleagues to work daily.", false, R.drawable.transport_habit));
         habits.add(new Habit("4","Food","small","Did you eat BLAH BLAH today?","Eating BLAH BLAH daily.", false,R.drawable.transport_habit));
         habits.add(new Habit("5","Food","medium","Did you eat BLAH today?","Eating BLAH daily.", false, R.drawable.transport_habit));
+        habits.add(new Habit("1","Transportation","large","Did you run walk or bike to work?","Walking and biking to work daily.", false, R.drawable.transport_habit));
+        habits.add(new Habit("2","Transportation","medium","Did you take the bus to work?","Taking the bus to work daily.", false, R.drawable.transport_habit));
+        habits.add(new Habit("3","Transportation","small","Did you run carpool to work?","Carpooling with colleagues to work daily.", false, R.drawable.transport_habit));
+        habits.add(new Habit("4","Food","small","Did you eat BLAH BLAH today?","Eating BLAH BLAH daily.", false,R.drawable.transport_habit));
+        habits.add(new Habit("5","Food","medium","Did you eat BLAH today?","Eating BLAH daily.", false, R.drawable.transport_habit));
+        habits.add(new Habit("1","Transportation","large","Did you run walk or bike to work?","Walking and biking to work daily.", false, R.drawable.transport_habit));
+        habits.add(new Habit("2","Transportation","medium","Did you take the bus to work?","Taking the bus to work daily.", false, R.drawable.transport_habit));
+        habits.add(new Habit("3","Transportation","small","Did you run carpool to work?","Carpooling with colleagues to work daily.", false, R.drawable.transport_habit));
+        habits.add(new Habit("4","Food","small","Did you eat BLAH BLAH today?","Eating BLAH BLAH daily.", false,R.drawable.transport_habit));
+        habits.add(new Habit("5","Food","medium","Did you eat BLAH today?","Eating BLAH daily.", false, R.drawable.transport_habit));
+
 
 
         recyclerView.setLayoutManager(new LinearLayoutManager(this));
         habitadaptor = new HabitAdapter(getApplicationContext(), habits);
         recyclerView.setAdapter(habitadaptor);
 
+    }
+
+    private void filterList(CheckBox transport, CheckBox food, CheckBox electricity, CheckBox shopping, CheckBox small, CheckBox medium, CheckBox large) {
+        ArrayList<String> type = new ArrayList<>();
+        ArrayList<String> impact = new ArrayList<>();
+        ArrayList<Habit> filtered = new ArrayList<>();
+
+        if (transport.isChecked()){type.add("Transportation");}
+        if (food.isChecked()){type.add("Food");}
+        if (electricity.isChecked()){type.add("Electricity");}
+        if (shopping.isChecked()){type.add("Shopping");}
+        if (small.isChecked()){impact.add("small");}
+        if (medium.isChecked()){impact.add("medium");}
+        if (large.isChecked()){impact.add("large");}
+
+        for (Habit habit : habits){
+            if (type.contains(habit.type) || impact.contains(habit.impact)){
+                filtered.add(habit);
+            }
+        }
+
+        if (filtered.isEmpty()) {
+            Toast.makeText(this, "Not Results Found", Toast.LENGTH_SHORT).show();
+        } else {
+            habitadaptor.setFilteredList(filtered);
+            recyclerView.setAdapter(habitadaptor);
+        }
     }
 
     private void searchList(String newText) {
@@ -65,7 +127,6 @@ public class HabitSearchActivity extends AppCompatActivity {
         if (searched.isEmpty()) {
             Toast.makeText(this, "Not Results Found", Toast.LENGTH_SHORT).show();
         } else {
-            Toast.makeText(this, "Here are your results", Toast.LENGTH_SHORT).show();
             habitadaptor.setFilteredList(searched);
             recyclerView.setAdapter(habitadaptor);
         }
